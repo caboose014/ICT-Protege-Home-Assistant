e75295a9183761e5c57bb2323083056eb431fce2
Jordan McDonald <j_14@me.com>
2026-02-16 18:51:17 +1300

    Refactor constants in const.py for clarity and new features
    Updated configuration constants for ICT Automation integration, including renaming keys and adding new constants for arming modes.

--- custom_components/ict_automation/const.py
+++ custom_components/ict_automation/const.py
@@ -1,14 +1,18 @@
-"""Constants for the ICT Automation integration."""
 DOMAIN = "ict_automation"
+
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_PASSWORD = "password"
-CONF_DOORS = "doors_config"
-CONF_AREAS = "areas_config"
-CONF_INPUTS = "inputs_config"
-CONF_TROUBLES = "troubles_config"
-CONF_OUTPUTS = "outputs_config"
-PKT_TYPE_LOGIN = 0x01
-PKT_TYPE_STATUS = 0x03
-PKT_TYPE_CONTROL = 0x04
-PKT_TYPE_EVENT = 0x05
+
+CONF_DOORS = "doors"
+CONF_AREAS = "areas"
+CONF_INPUTS = "inputs"
+CONF_OUTPUTS = "outputs"
+# Troubles are now part of inputs, but we keep the key just in case of legacy usage
+CONF_TROUBLES = "troubles" 
+
+# New Constants for Arming Modes
+CONF_ENABLE_AWAY = "enable_arm_away"
+CONF_ENABLE_STAY = "enable_arm_stay"
+CONF_ENABLE_NIGHT = "enable_arm_night"
+CONF_ENABLE_BYPASS = "enable_arm_bypass"
